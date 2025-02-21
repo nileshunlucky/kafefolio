90cc337808fccc264e7a9248ff30b8f1af4dfb39
unluckynilesh <nileshinde001@gmail.com>
2025-02-21 10:56:21 +0530

    razorpay veryToken

--- server/routes/razorpay.routes.js
+++ server/routes/razorpay.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { subscription } from "../controllers/razorpay.controller.js";
+import { verifyToken } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // Endpoint to create a subscription
-router.post("/create-subscription", subscription);
+router.post("/create-subscription", verifyToken, subscription);
 
 export default router;
